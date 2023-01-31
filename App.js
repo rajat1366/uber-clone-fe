0e9be92f5a4918061e50e921eecc151d5b3c5426
Rajat Kumar <rajat1366@gmail.com>
2023-02-01 00:01:00 +0530

    intall redux and create navSlice

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Provider } from 'react-redux';
+import { store } from './store';
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app! Rajat </Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+        <View className="flex-1 items-center justify-center bg-white">
+          <Text>Open up App.js to HELL </Text>
+          <StatusBar style="auto" />
+        </View>
+    </Provider>
+    
   );
 }
